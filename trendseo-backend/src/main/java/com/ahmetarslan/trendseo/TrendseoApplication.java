a16775c462eda41c7a98626c6ea4b85f456ef55b
Ahmet Arslan <ahmet.arssllan@gmail.com>
2026-07-20 15:57:09 +0300

    chore: initial Spring Boot skeleton

---
+++ trendseo-backend/src/main/java/com/ahmetarslan/trendseo/TrendseoApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetarslan.trendseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendseoApplication.class, args);
+	}
+
+}
---
+++ trendseo-backend/src/test/java/com/ahmetarslan/trendseo/TrendseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetarslan.trendseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
